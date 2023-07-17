d39e0f3fa553a7f556ac2e51d9e0cc8f51883d7f
PyIsaac <maka052871@yahoo.com>
2023-07-17 20:11:19 +0300

    posts

---
+++ django_project/users/signals.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+		instance.profile.save()
+
+
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from .models import Credit
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def add_initial_credit(sender, instance, created, **kwargs):
+    if created:
+        credit = Credit(user=instance, score=settings.INITIAL_CREDIT)
+        credit.save()
